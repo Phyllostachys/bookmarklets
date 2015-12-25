96ddb13a12f3296ed863fd5fa1611589ed28265a
Jacob Shaffer <shaffer_jacob@hotmail.com>
2015-12-25 04:24:18 -0800

    added Code Reddit bookmarklet

---
+++ CodeReddit_bookmarklet.js
@@ -0,0 +1,1 @@
+javascript:%20(function()%20{window.open("http://codereddit.com/?topic="%20+%20window.getSelection());}());
---
+++ CodeReddit_code.js
@@ -0,0 +1,3 @@
+(function() {
+    window.open("http://codereddit.com/?topic=" + window.getSelection());
+}());
